5aabdfa03e8a50750f7aef775136709d18b46329
ManjeshC9 <120353457+ManjeshC9@users.noreply.github.com>
2023-01-10 00:37:07 +0530

    q3.py

---
+++ q3.py
@@ -0,0 +1,14 @@
+i = 1
+while i<10:
+  i+=1
+  a=random.randint(0,10)
+  b=random.randint(0,10)
+  print("question",i,"=",a,"*",b)
+  ans=int(input("Enter your number")
+          
+          if ans==a*b:
+          print("correct number")
+          else:
+          print("incorrect number, the correct answer is",a*b)
+          
+                

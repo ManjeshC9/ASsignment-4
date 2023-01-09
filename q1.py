526735e69bcf39a8987292231f37020aa1898323
ManjeshC9 <120353457+ManjeshC9@users.noreply.github.com>
2023-01-10 00:22:31 +0530

    q1.py

---
+++ q1.py
@@ -0,0 +1,13 @@
+x = int(input("Enter Marks"))
+if(x<25):
+     print("Grade F")
+elif(x<=45):
+    print("Grade E")
+elif(x<=50):
+    print("Grade D")
+elif(x<=60):
+    print("Grade C")
+elif(x<=80):
+    print("Grade B")
+else:
+    print(" Grade A")
